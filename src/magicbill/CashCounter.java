b5cbfe383aeb7e6d8cc62aafd758d566862d8293
Mubeen-Zaki <mubeenzaki31@gmail.com>
2022-05-14 18:57:36 +0530

    first commit

---
+++ src/magicbill/CashCounter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magicbill;
+
+/**
+ *
+ * @author Hameed Ahmed
+ */
+public class CashCounter {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mfobj=new MainFrame();
+    mfobj.setVisible(true);
+    
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/magicbill/GlobalClass.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magicbill;
+
+/**
+ *
+ * @author Hameed Ahmed
+ */
+public class GlobalClass {
+    
+    public static int iprs1;
+    public static int iprs2;
+    public static int iprs5;
+    public static int iprs10;
+    public static int iprs10coin;
+    public static int iprs20;
+    public static int iprs50;
+    public static int iprs100;
+    public static int iprs200;
+    public static int iprs500;
+    public static int iprs2000;
+    public static int invamt;
+    public static int ipamt;
+    
+    
+    
+    
+    public static int oprs1;
+    public static int oprs2;
+    public static int oprs5;
+    public static int oprs10;
+    public static int oprs10coin;
+    public static int oprs20;
+    public static int oprs50;
+    public static int oprs100;
+    public static int oprs200;
+    public static int oprs500;
+    public static int oprs2000;
+    
+    
+    
+    
+}
